5d0a20db3f6b05fbb05a3552815b3561c61d0df1
Abhishek Vadnerkar <abhishek@mightycast.com>
2016-05-08 12:00:50 -0400

    Changed titles to be less than 32 chars

---
+++ app/src/main/java/physiotherapy/mcgill/com/painfree/Utilities/DBColumn.java
@@ -0,0 +1,15 @@
+package physiotherapy.mcgill.com.painfree.Utilities;
+
+/**
+ * Created by Abhishek Vadnerkar on 16-05-08.
+ */
+public class DBColumn {
+
+    public String header;
+    public String format;
+
+    public DBColumn(String header, String format){
+        this.header = header;
+        this.format = format;
+    }
+}
